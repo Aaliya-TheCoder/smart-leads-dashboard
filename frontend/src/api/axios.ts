3dc722321c77f0d114f2e073888745ed878e2ffb
Aaliy-TheCoder <aaliya969392@gmail.com>
2026-05-30 21:02:25 +0530

    Updated vercel URL

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -7,7 +7,7 @@ console.log("API URL:", import.meta.env.VITE_API_URL);
 //     : "http://localhost:5000/api",
 // });
 const API = axios.create({
-  baseURL: "https://smart-leads-dashboard-pfne.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
 API.interceptors.request.use(
